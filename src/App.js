d375ac907ee90205b6a2acde22a6183460866256
ryankayne <coope1rk@gmail.com>
2019-12-12 14:59:40 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { addFeature, removeFeature } from './redux/actions/action';
 
 
 const App = (props) => {
   
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
+  // const removeFeature = item => {
+  //   // dispatch an action here to remove an item
+  // };
 
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
+  // const addFeature = item => {
+  //   // dipsatch an action here to add an item
+  // };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <AddedFeatures car={props.car} removeFeature={props.removeFeature}/>
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} addFeature={props.addFeature} />
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -33,8 +34,17 @@ const App = (props) => {
 
 const mapStateToProps = state => {
   return {
-    ...state
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice,
+    car: state.car
   }
 }
 
-export default connect(mapStateToProps, {})(App);
+const mapDispatchToProps = {
+  addFeature,
+  removeFeature
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)(App);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { statement } from '../../../../../../../AppData/Local/Microsoft/TypeScript/3.6/node_modules/@types/babel__template';
+
 
 const AdditionalFeature = props => {
   return (
